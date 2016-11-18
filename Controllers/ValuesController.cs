3dd14174d1429cd8382f388c542494551e79a25b
jessybalanag@gmail.com <jessybalanag@gmail.com>
2016-11-18 17:47:56 +0800

    Updated SDK Version. Added Values Controller

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace hallotapco.Controllers
+{
+    [Route("api/TAPCO")]
+    public class ValuesController : Controller
+    {
+
+       
+
+        [HttpGet]
+        public  Message Get()
+        {
+            return new Message() { text = "Hallo Tapco!" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value: " + id;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,13 +1,30 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Nancy.Owin;
- 
+
 namespace TapcoApiApplication
 {
    public class Startup
     {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
         public void Configure(IApplicationBuilder app)
         {
-            app.UseOwin(x => x.UseNancy());
+            //    app.UseOwin(x => x.UseNancy());
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+
         }
    }
 }
